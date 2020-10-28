54f527964ad89ebf605a021dd899b10215ec9fb7
jacobramsay <jacob.ramsay@mffp.gouv.qc.ca>
2020-10-28 14:31:24 -0400

    Changement des fichiers

--- plugins/quikkly/ngx/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { IonicNativePlugin } from '@ionic-native/core';
-/**
- * @name QuikklyPlugin
- * @description ionic wrapper for cordova-plugin-quikkly
- * Use the quikkly scanner in your ionic app
- */
-export declare class QuikklyPlugin extends IonicNativePlugin {
-    openScanner(): Promise<string[]>;
-}
